53c0ac599fd5c62941619cf73f0a97d47b3bfc69
Pedro Ferreira <prgf2011@gmail.com>
2023-09-18 10:51:42 +0100

    add navivagtion and conditional rendering

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import react, { useContext } from "react";
+
+const Stack = createNativeStackNavigator();
+
+const login = false; //change this later to context
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* if user is not logged in, show login screeen */}
+      <Stack.Navigator initialRouteName="Login">
+        {!login ? (
+          <Stack.Group>
+            <Stack.Screen name="Login" component={Login} />
+          </Stack.Group>
+        ) : (
+          <Stack.Group>
+            {/* if user is logged in, show home screeen */}
+
+            <Stack.Screen name="Home" component={Home} />
+          </Stack.Group>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
---
+++ pages/Home.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Layout from "../components/modules/Layout";
+
+export default function Home() {
+  return (
+    <View>
+      <Layout>
+        <Text>Home</Text>
+      </Layout>
+    </View>
+  );
+}
