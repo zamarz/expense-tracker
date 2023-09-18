c668b534698196a337ba8bcfc8381edc850b120d
zamarz <zachmarzouk@gmail.com>
2023-09-18 13:51:47 +0100

    "Add header in navigation"

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
-import { StyleSheet } from "react-native";
+import { Image, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import react, { useContext } from "react";
 
 const Stack = createNativeStackNavigator();
 
-const login = false; //change this later to context
+const login = true; //change this later to context
 
 export default function App() {
   return (
     <NavigationContainer>
       {/* if user is not logged in, show login screeen */}
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#f4511e",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
         {!login ? (
           <Stack.Group>
             <Stack.Screen name="Login" component={Login} />
@@ -22,7 +32,19 @@ export default function App() {
           <Stack.Group>
             {/* if user is logged in, show home screeen */}
 
-            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                title: "My Home",
+                headerLeft: () => (
+                  <Image
+                    style={{ width: 50, height: 50 }}
+                    source={require("./assets/android-chrome-192x192.png")}
+                  />
+                ),
+              }}
+            />
           </Stack.Group>
         )}
       </Stack.Navigator>
--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import NavMenu from "../navigation/NavMenu";
 
 export default function Header() {
   return (
-    <View>
+    <View styles={styles.container}>
       <Text>Header</Text>
       <NavMenu />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
