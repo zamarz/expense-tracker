56071f3e6427c621e653900047bfa9dc91b0a629
Pedro Ferreira <50713178+prgf87@users.noreply.github.com>
2023-09-18 13:43:33 +0100

    Revert "user auth done, login + redirect"

--- App.js
+++ App.js
@@ -3,42 +3,27 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
-import { useEffect, useState } from "react";
-import { authFire } from "./firebaseConfig";
-import { onAuthStateChanged } from "firebase/auth";
+import react, { useContext } from "react";
 
 const Stack = createNativeStackNavigator();
-const HomeStack = createNativeStackNavigator();
-const LoginStack = createNativeStackNavigator();
 
 const login = false; //change this later to context
 
 export default function App() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    onAuthStateChanged(authFire, (user) => {
-      console.log(user, "#####USER####");
-      setUser(user);
-    });
-  });
-
   return (
     <NavigationContainer>
       {/* if user is not logged in, show login screeen */}
       <Stack.Navigator initialRouteName="Login">
-        {!user ? (
-          <LoginStack.Group>
-            <LoginStack.Screen name="Login" component={Login} />
-            <LoginStack.Screen name="Register" component={Register} />
-          </LoginStack.Group>
+        {!login ? (
+          <Stack.Group>
+            <Stack.Screen name="Login" component={Login} />
+          </Stack.Group>
         ) : (
-          <HomeStack.Group>
+          <Stack.Group>
             {/* if user is logged in, show home screeen */}
 
-            <HomeStack.Screen name="Home" component={Home} />
-          </HomeStack.Group>
+            <Stack.Screen name="Home" component={Home} />
+          </Stack.Group>
         )}
       </Stack.Navigator>
     </NavigationContainer>
