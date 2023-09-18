4c1675c5af1e1fe224d71935e3a529b72372d3c4
Pedro Ferreira <prgf2011@gmail.com>
2023-09-18 13:45:35 +0100

    new commit

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2W2PaEp-hSusUIHq1QBhqrchnD2QL23w",
+  apiKey: process.env.FIREBASE_API,
   authDomain: "test2-c87e5.firebaseapp.com",
   databaseURL:
     "https://test2-c87e5-default-rtdb.europe-west1.firebasedatabase.app",
