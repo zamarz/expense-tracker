ee782dd819194630792661caa83e98286cc5df9c
isabelpopa <isabel.popa@outlook.com>
2023-09-21 11:02:30 +0100

    Data stored locally, not rendered from database

--- components/account/AccountsAdder.jsx
+++ components/account/AccountsAdder.jsx
@@ -1,41 +1,83 @@
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { SafeAreaView, StyleSheet, View, TextInput, Button } from "react-native";
 import { Loading } from "../loading/Loading";
+import { addDoc, collection } from "firebase/firestore";
+import { dbFire, authFire } from "../../firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
 
-export default function AccountsAdder() {
+export default function AccountsAdder({ navigation }) {
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
     const [bank, setBank] = useState("");
-    const [balance, setBalance] = useState(0);
+    const [balance, setBalance] = useState(null);
+    const [type, setType] = useState("");
+    const [budget, setBudget] = useState(null);
+    const [userId, setUserId] = useState("");
 
     if (isLoading) return <Loading />;
     if (isError) return <p>Something went wrong!</p>;
 
+    const auth = authFire;
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            const uid = user.uid
+            setUserId(uid)
+        }
+    })
+
+    const handleAddAccount = async () => {
+        const newAccount = {
+            bank: bank,
+            balance: balance,
+            type: type,
+            budget: budget,
+            userId: userId,
+        };
+
+        await addDoc(collection(dbFire, "account"), newAccount)
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View>
-                <Layout>
-                    <View>
-                        <TextInput
-                            placeholder="Account bank"
-                            value={password}
-                            onChangeText={text => setBank(text)}
-                            style={styles.input}
-                        />
-                        <TextInput
-                            placeholder="Account balance"
-                            value={password}
-                            onChangeText={text => setBalance(text)}
-                            style={styles.input}
-                        />
-                        <Button
-                        // Add functionality for account adder button
-                            onPress={() => { }}
-                            title="Add account"
-                            accessibilityLabel="Add a new account to the accounts list"
-                        ></Button>
-                    </View>
-                </Layout>
+                <TextInput
+                    placeholder="Bank name"
+                    value={bank}
+                    onChangeText={text => setBank(text)}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Balance"
+                    value={balance}
+                    onChangeText={text => setBalance(text)}
+                    keyboardType="numeric"
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Type"
+                    value={type}
+                    onChangeText={text => setType(text)}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Budget"
+                    value={budget}
+                    onChangeText={text => setBudget(text)}
+                    keyboardType="numeric"
+                    style={styles.input}
+                />
+                <Button
+                    // Add functionality for account adder button
+                    onPress={() => { handleAddAccount() }}
+                    title="Add account"
+                    accessibilityLabel="Add a new account to the accounts list"
+                ></Button>
+                <Button onPress={() => navigation.navigate('Home')}
+                    title="Go back"
+                    accessibilityLabel="Button to navigate to Home page"></Button>
             </View>
         </SafeAreaView>
     );
--- components/account/AccountsList.jsx
+++ components/account/AccountsList.jsx
@@ -1,30 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, FlatList, StyleSheet, Text, View, Button } from "react-native";
+import AccountsCard from "./AccountsCard";
+import { doc, deleteDoc } from "firebase/firestore";
+import { dbFire } from "../../firebaseConfig";
 
 const accountsData = [
     {
         id: "1",
         bank: "Halifax",
         balance: 5.25,
+        budget: "0",
+        cardType: "debit",
+        income: "0",
     },
     {
         id: "2",
         bank: "Santander",
         balance: 155,
+        budget: "0",
+        cardType: "debit",
+        income: "0",
     },
     {
         id: "3",
         bank: "Natwest",
         balance: 56.50,
+        budget: "0",
+        cardType: "debit",
+        income: "0",
     },
     {
         id: "4",
         bank: "Chase",
         balance: 750.85,
+        budget: "0",
+        cardType: "debit",
+        income: "0",
+    },
+    {
+        id: "5",
+        bank: "Lloyds",
+        balance: -50,
+        budget: "0",
+        cardType: "credit",
+        income: "0",
     },
 ];
 
-export default function AccountList() {
+export default function AccountList({ navigation }) {
+    const [accounts, setAccounts] = useState(accountsData);
     const calculateTotalBalance = () => {
         let totalBalance = 0;
         for (const account of accountsData) {
@@ -35,20 +59,68 @@ export default function AccountList() {
 
     const totalBalance = calculateTotalBalance();
 
+    const handleEditBudget = (accountId, newBudget) => {
+        const updatedAccounts = accounts.map((account) => {
+            if (account.id === accountId) {
+                account.budget = newBudget;
+            }
+            return account;
+        })
+        setAccounts(updatedAccounts)
+     };
+
+    const handleEditBalance = (accountId, newBalance) => {
+        const updatedAccounts = accounts.map((account) => {
+            if (account.id === accountId) {
+                account.balance = newBalance;
+            }
+            return account;
+        })
+        setAccounts(updatedAccounts);
+    };
+
+    const handleAddIncome = (accountId, newIncome) => {
+        const updatedAccounts = accounts.map((account) => {
+            if (account.id === accountId) {
+                account.income = newIncome;
+            }
+            return account;
+        })
+        setAccounts(updatedAccounts)
+    };
+
+    const handleDeleteAccount = async () => {
+        await deleteDoc(doc(dbFire, "account", "accountId"))
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
     return (
         <SafeAreaView style={styles.container}>
             <View>
                 <Text style={styles.title}>Total Accounts Balance: £{totalBalance.toFixed(2)}</Text>
             </View>
+            <View>
+                <Text style={styles.title}>Total Budget: £0</Text>
+            </View>
             <FlatList
+                contentContainerStyle={{ alignSelf: 'flex-start' }}
+                showsVerticalScrollIndicator={true}
+                showsHorizontalScrollIndicator={true}
                 data={accountsData}
-                renderItem={({ item }) => <Text style={styles.item}>{item.bank}: £{item.balance}</Text>}
+                renderItem={({ item }) => <AccountsCard item={item} onDelete={() => handleDeleteAccount(item.id)} onEditBudget={() => handleEditBudget(item.id)} onEditBalance={() => handleEditBalance(item.id)} onAddIncome={() => handleAddIncome(item.id)} />}
                 keyExtractor={(item) => item.id}
             />
             <View>
                 <Button onPress={() => navigation.navigate('AccountsAdder')}
-                    title="Add account"
+                    title="Add new account"
                     accessibilityLabel="Add a new account to the accounts list"></Button>
+            </View>
+            <View>
+                <Button onPress={() => navigation.navigate('Home')}
+                    title="Go back"
+                    accessibilityLabel="Button to navigate to Home page"></Button>
             </View>
         </SafeAreaView>
     );
