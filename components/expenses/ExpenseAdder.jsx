714af5fc0b54e0745e6687c0a327cda96ea2a4f7
zamarz <zachmarzouk@gmail.com>
2023-09-22 14:22:13 +0100

    "Fix expense adder"

--- components/expenses/ExpenseAdder.jsx
+++ components/expenses/ExpenseAdder.jsx
@@ -5,11 +5,19 @@ import {
   Button,
   Text,
   SafeAreaView,
+  TouchableOpacity,
+  Pressable,
 } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { authFire, dbFire } from "../../firebaseConfig";
 import { addDoc, collection } from "@firebase/firestore";
-import { getCurrentUserId } from "../../utils/helpers";
+import { onAuthStateChanged } from "@firebase/auth";
+import ErrorHandler from "../error/ErrorHandler";
+import { Loading } from "../loading/Loading";
+import { Formik } from "formik";
+import { stringifyValueWithProperty } from "react-native-web/dist/cjs/exports/StyleSheet/compiler";
+import { object, string, number } from "yup";
+import * as yup from "yup";
 
 export default function ExpenseAdder({ navigation }) {
   const [amount, setAmount] = useState("");
@@ -20,6 +28,17 @@ export default function ExpenseAdder({ navigation }) {
   const [account, setAccount] = useState("");
   const [location, setLocation] = useState("");
   const [userId, setUserId] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({});
+
+  const auth = authFire;
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      setUserId(uid);
+    }
+  });
 
   const expenses = {
     amount,
@@ -32,94 +51,146 @@ export default function ExpenseAdder({ navigation }) {
     userId,
   };
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
-    const userId = getCurrentUserId();
-    setUserId(userId);
-    console.log(userId);
+  const expenseSchema = yup.object().shape({
+    amount: yup
+      .number("")
+      .typeError("Amount should be a number")
+      .required("Amount is required!")
+      .positive()
+      .test(
+        "maxDigitsAfterDecimal",
+        "number field must have 2 digits after decimal or less",
+        (number) => /^\d+(.\d{1,2})?$/.test(number)
+      ),
+    merchant: yup.string().required(),
+    account: yup.string().required(),
+    location: yup.string().required(),
+    category: yup.string().required(),
+    date: yup.string().required(),
+  });
+  //Need to change location, category and date
+
+  const handleSubmit = async (values) => {
+    values.userId = expenses.userId;
+    //preventDefault();
+    console.log(values);
+    setLoading(true);
     try {
-      //   setDate(Date());
-      //   navigator.geolocation.getCurrentPosition((position) => {
-      //     setLocation(
-      //       `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`
-      //     );
-      //   });
-      // const res = await addDoc(collection(dbFire, "expenses"), expenses);
-      // console.log(res, "#####response#####");
+      const res = await addDoc(collection(dbFire, "expenses"), values);
+      setLoading(false);
+      //To add submission message state
     } catch (error) {
-      console.log(error);
+      setError(error);
+      setLoading(false);
     }
+    // setLoading(true);
+    // try {
+    //   setDate("Today");
+    //   setLocation("Here");
+    //   const res = await addDoc(collection(dbFire, "expenses"), expenses);
+    //   console.log(res, "#####response#####");
+    //   setLoading(false);
+    //   console.log("here!!");
+    // } catch {
+    //   setError(error);
+    //   setLoading(false);
+    // }
   };
 
-  useEffect(() => {
-    getCurrentUserId();
-  }, []);
+  if (error) return <ErrorHandler error={error} />;
 
   return (
-    <View style={styles.container}>
-      <Text>Add a new expense by filling in the fields below</Text>
-      <SafeAreaView style={styles.inputContainer}>
-        <TextInput
-          placeholder="Amount"
-          value={amount}
-          onChangeText={(text) => setAmount(text)}
-          style={styles.input}
-          inputMode="decimal"
-        />
-        <TextInput
-          placeholder="Merchant"
-          value={merchant}
-          onChangeText={(text) => setMerchant(text)}
-          style={styles.input}
-          inputMode="text"
-        />
-        <TextInput
-          placeholder="Category"
-          value={category}
-          onChangeText={(text) => setCategory(text)}
-          style={styles.input}
-        />
-        <TextInput
-          placeholder="Account"
-          value={account}
-          onChangeText={(text) => setAccount(text)}
-          style={styles.input}
-        />
-        <TextInput
-          placeholder="Date"
-          value={date}
-          onChangeText={(text) => setDate(text)}
-          style={styles.input}
-        />
-        <TextInput
-          placeholder="Location"
-          value={location}
-          onChangeText={(text) => setLocation(text)}
-          style={styles.input}
-        />
-        <Button
-          title="Add Receipt"
-          onPress={() => {
-            alert("Need to add navigation to ExpenseAdder");
-            setReceipt(receipt); //make this do something
-          }}
-        />
-        <Button
-          title="Add New Expense"
-          onPress={(e) => {
-            // alert("Link to Receipt Scanner");
-            // setReceipt(true);
-
-            submitHandler(e);
-          }}
-        />
-      </SafeAreaView>
-      <Button
-        onPress={() => navigation.navigate("Home")}
-        title="Go back"
-        accessibilityLabel="Go back to Home"
-      />
-    </View>
+    <Formik
+      initialValues={{
+        amount: "",
+        category: "",
+        merchant: "",
+        date: "",
+        receipt: "",
+        account: "",
+        location: "",
+      }}
+      validationSchema={expenseSchema}
+      onSubmit={(values) => {
+        //?mutating state?? Refactor this
+        setFormData(
+          (formData.amount = values.amount),
+          (formData.category = values.category),
+          (formData.account = values.account),
+          (formData.date = values.date),
+          (formData.merchant = values.merchant),
+          (formData.receipt = values.receipt),
+          (formData.location = values.location)
+        );
+        console.log(formData);
+        handleSubmit(formData);
+      }}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values, errors }) => {
+        return (
+          <View style={styles.container}>
+            <View style={styles.inputRow}>
+              <Text>Add a new Expense</Text>
+              <TextInput
+                aria-label="Amount"
+                style={styles.input}
+                onChangeText={handleChange("amount")}
+                onBlur={handleBlur("amount")}
+                value={values.amount}
+                placeholder="Amount"
+              />
+              {errors.amount && <Text>{errors.amount}</Text>}
+            </View>
+            <TextInput
+              aria-label="Merchant"
+              style={styles.input}
+              onChangeText={handleChange("merchant")}
+              onBlur={handleBlur("merchant")}
+              value={values.merchant}
+              placeholder="Merchant"
+            />
+            {errors.merchant && <Text>{errors.merchant}</Text>}
+            <TextInput
+              aria-label="Category"
+              placeholder="Category"
+              style={styles.input}
+              onChangeText={handleChange("category")}
+              onBlur={handleBlur("category")}
+              value={values.category}
+            />
+            {errors.category && <Text>{errors.category}</Text>}
+            <TextInput
+              aria-label="Account"
+              placeholder="Account"
+              style={styles.input}
+              onChangeText={handleChange("account")}
+              onBlur={handleBlur("account")}
+              value={values.account}
+            />
+            {errors.account && <Text>{errors.account}</Text>}
+            <TextInput
+              aria-label="Date"
+              placeholder="Date"
+              style={styles.input}
+              onChangeText={handleChange("date")}
+              onBlur={handleBlur("date")}
+              value={values.date}
+            />
+            {errors.date && <Text>{errors.date}</Text>}
+            <TextInput
+              aria-label="Location"
+              placeholder="Location"
+              style={styles.input}
+              onChangeText={handleChange("location")}
+              onBlur={handleBlur("location")}
+              value={values.location}
+            />
+            {errors.location && <Text>{errors.location}</Text>}
+            <Button title="Submit" onPress={handleSubmit} />
+          </View>
+        );
+      }}
+    </Formik>
   );
 }
 
