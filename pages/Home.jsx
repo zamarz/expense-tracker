931c8ccab908e0a67dc02f3f0d9ec5c56cd620b2
zamarz <zachmarzouk@gmail.com>
2023-09-25 14:04:09 +0100

    "Add styling for home and budgets"

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,51 +1,75 @@
-import { View, Text, Button, StyleSheet, SafeAreaView } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import React, { useContext } from "react";
 import Logout from "../components/buttons/Logout";
 import ExpenseListHome from "../components/expenses/ExpenseListHome";
 import BudgetPlanner from "../components/budget/BudgetPlanner";
 import { UserContext } from "../context/UserContext";
 import { AppTracker } from "../context/AppTracker";
+import { Button, Divider, useTheme } from "react-native-paper";
 
 export default function Home({ navigation }) {
   const user = useContext(UserContext);
   const { balance, expenses } = useContext(AppTracker);
+
+  const theme = useTheme();
 
   if (user) {
     return (
       <SafeAreaView style={styles.container}>
         <View>
           <View>
-            <Text style={styles.title}>Balance: {balance}</Text>
+            <Text variant="headlineLarge" style={styles.title}>
+              Balance: {balance}
+            </Text>
+            <Divider />
           </View>
           <View>
             <BudgetPlanner expenses={expenses} />
+            <Divider />
           </View>
           <View>
             <ExpenseListHome expenses={expenses} />
+            <Divider />
           </View>
           <View>
             <Button
+              style={styles.appButtonContainer}
+              mode="contained"
               onPress={() =>
                 navigation.navigate("Expense List", { screen: "ExpenseList" })
               }
               title="Expenses List"
               accessibilityLabel="Goes to the expenses page"
-            />
+              icon="cash"
+            >
+              Expenses List{" "}
+            </Button>
+            <Divider />
             <Button
+              style={styles.appButtonContainer}
+              mode="contained"
               onPress={() => {}}
               title="Scan expense"
               accessibilityLabel="Add a new expense to an account by scanning a receipt"
-            />
+            >
+              Scan expense
+            </Button>
+            <Divider />
+
             {/* <Button
               onPress={handleAppDemo}
               title="App Demo"
               accessibilityLabel="Learn more about how to use the expense tracker app, through our app demo"
             /> */}
             <Button
+              style={styles.appButtonContainer}
+              mode="contained"
               onPress={() => navigation.navigate("Accounts List")}
               title="View / Add Account"
               accessibilityLabel="View a list of accounts or add a new account"
-            />
+            >
+              Add Account
+            </Button>
             <Logout />
           </View>
         </View>
@@ -66,5 +90,11 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     borderBottomColor: "#737373",
     borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  appButtonContainer: {
+    elevation: 8,
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
   },
 });
