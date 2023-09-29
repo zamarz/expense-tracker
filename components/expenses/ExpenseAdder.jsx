4d870911c6196e8e2252980ccce55c2d2dd8520b
Pedro Ferreira <prgf2011@gmail.com>
2023-09-29 09:39:58 +0100

    fixes loading state

--- components/expenses/ExpenseAdder.jsx
+++ components/expenses/ExpenseAdder.jsx
@@ -166,6 +166,7 @@ const ExpenseAdder = ({ navigation }) => {
     if (location !== undefined && geolocation !== undefined) {
       addExpense(formData)
         .then(() => {
+          setLoading(false);
           alert(
             "Expense Added",
             `You have successfully added your expense for the amount of £${amount}`,
@@ -186,8 +187,7 @@ const ExpenseAdder = ({ navigation }) => {
           setGeolocation(undefined);
           setLocation(undefined);
           setMerchant("");
-          setLoading(false);
-          navigation.navigate("Expense List", { screen: "ExpenseList" });
+          navigation.navigate("Expense List", { screen: "Expenses List" });
         })
         .catch((err) => {
           console.log(err);
